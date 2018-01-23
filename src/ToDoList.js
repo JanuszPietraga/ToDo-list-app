ebc9abbe9f0cf8bf24f2a9edab9f433681772244
Janusz <januszpietraga@gmail.com>
2018-01-23 22:13:17 +0100

    dodanie pierwszego komponentu

---
+++ src/ToDoList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class ToDoList extends Component {
+    render () {
+        return (
+            <h1>trzeba zrobic</h1>
+        )
+    }
+}
+export default ToDoList
