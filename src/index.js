44827fa0c677b49faaf229b8af57d1cde3dc1c40
Janusz <januszpietraga@gmail.com>
2018-01-24 09:09:55 +0100

    firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import setupFirebase from './setupFirebase'
+setupFirebase()
 
 ReactDOM.render(
     <App />,
